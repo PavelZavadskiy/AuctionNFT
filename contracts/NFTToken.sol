1b568475e5d6a66b9d9f1c20feadefda774ee8c3
PavelZavadskiy <87939143+PavelZavadskiy@users.noreply.github.com>
2021-09-23 18:59:11 +0300

    Update NFTToken.sol

--- contracts/NFTToken.sol
+++ contracts/NFTToken.sol
@@ -4,12 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-//Благодаря ERC20 нам нет необходимости описывать свою реализацию интерфейса IERC20
-// totalSupply balanceOf transfer transferFrom approve allowance
 contract CERC20 is ERC20("AuctionToken", "AUT") {
-    constructor(){
-        mint(msg.sender, 1000000 ether); 
-    }
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
